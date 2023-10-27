9ddf4e72d41c28b27a9ccc45819ac31979768d3c
Doreen <doreenmarie_cee@hotmail.com>
2023-10-27 21:52:52 +0200

    iterations completed

--- routes/login.routes.js
+++ routes/login.routes.js
@@ -8,6 +8,38 @@ router.get('/login', (req, res) => {
     res.render('login');
 })
 
+router.post('/login',(req, res) => {
+    const { username, password} = req.body;
+
+    if(!username || !password) {
+        res.render('login', {errorMessage: 'Please enter valid credentials'});
+        return;
+    }
+
+    User.findOne({username})
+    .then(user => {
+        if(!user) {
+            res.render('login', {errorMessage: 'Invalid credentials, please try again'});
+            return;
+        }
+        if(bcrypt.compareSync(password, user.password)) {
+            res.redirect('/profile');
+        } else {
+            res.render('login', {errorMessage: 'Incorrect login credentials, try again'});
+        }
+
+    })
+    .catch(error => console.log(error));
+
+});
+
+
+
+
+
+
+
+
 
 
 module.exports = router;
